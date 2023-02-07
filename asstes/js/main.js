ae61490008c8ee183ea50c503be1e53182d4566d
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2023-02-07 19:04:36 +0700

    deploy code b1 js

---
+++ asstes/js/main.js
@@ -0,0 +1,70 @@
+// console.log("xin chao front-end 32!");
+
+// let name = "Nguyen Van A";
+// var age = "19";
+// const email = "nguyenvana@gmail.com";
+
+// console.log(name);
+// console.log(age);
+// console.log(email);
+
+// let city;
+// city = "Ha Noi";
+// console.log(city);
+// let City = "HCM";
+// console.log(City);
+
+
+// Kiem kieu du lieu cua bien
+// let number = 10;
+// console.log(typeof(number));
+
+// let email = "nguyenvana@gmail.com";
+// console.log(typeof(email));
+
+// Pham vi bien trong 1 function
+
+
+//Khai arrow function
+
+// const hello = () => {
+
+//     let name = "Hello Javascript Base";
+//     console.log(name);
+
+// }
+
+// ES5
+// let age = "100";
+
+// function hello() {
+
+//     name = "Hello Javascript Base";
+//     console.log(name);
+//     // console.log(age);
+
+// }
+
+// // hello();
+// // hello();
+// console.log(name);
+// // console.log(age);
+
+// Luu y trong string
+
+// let name = "Xin chao cac ban!." + " " +
+// " Minh la Thuan Nguyen. Huong dan lop minh front-end Basic!";
+// console.log(name);
+
+// const name = "Tuan Anh";
+// const age = "tuoi 20";
+
+// const description = name + " " + age;
+// console.log(description);
+
+// Teamplate String ES6
+const name = "Nguyen Van A";
+const age = 30;
+
+const fullName = `Toi ten la ${name}. Nam nay toi ${age}`;
+console.log(fullName);
